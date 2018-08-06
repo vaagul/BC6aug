f63e60f0dd2df69780b1c2cf8c16828a59e2cd2d
kushagra12 <vaish.kushagra@gmail.com>
2018-08-06 10:25:35 +0530

    Finished Comparable

--- src/Student.java
+++ src/Student.java
@@ -27,7 +27,32 @@ public class Student implements Comparable<Student>{
 
 
     @Override
-    public int compareTo(Student o) {
+    public int compareTo(Student s2) {
+        int greater = 0;
+        int lesser = 0;
+
+        if(s2.getMarksList().size() == this.getMarksList().size()){
+            for (int i = 0; i < this.getMarksList().size(); i++) {
+                int m1 = this.getMarksList().get(i);
+                int m2 = s2.getMarksList().get(i);
+
+                if(m1 > m2)
+                    greater++;
+                else if(m1 < m2)
+                    lesser++;
+                else
+                    return 0;
+
+                if(greater > 0 && lesser > 0)
+                    return 0;
+            }
+
+            if(greater == this.marksList.size())
+                return 1;
+
+            if(lesser == this.marksList.size())
+                return -1;
+        }
         return 0;
     }
 }
